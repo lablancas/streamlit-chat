5104843f48951c75bd70fb878c7ca4413561aea6
Lucas Blancas <lablancas@gmail.com>
2023-03-24 16:54:55 -0500

    ci: 🎡 use streamlit secrets instead of environment variables

--- index.py
+++ index.py
@@ -3,15 +3,10 @@ from langchain.vectorstores import Pinecone
 from langchain.llms import OpenAI
 from langchain.chains import VectorDBQA
 import pinecone
-import os
-import sys
-from dotenv import load_dotenv
 import openai
 import streamlit as st
 from streamlit_chat import message
 from uuid import uuid4
-
-load_dotenv()
 
 # exit if missing arguments
 if len(sys.argv) < 2:
@@ -19,13 +14,13 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 index_name = sys.argv[1]
-openai.organization = os.getenv("OPENAI_ORG_ID")
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.organization = st.secrets["OPENAI_ORG_ID"]
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 embeddings = OpenAIEmbeddings()
 llm = OpenAI()
 
 # initialize pinecone
-pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENVIRONMENT"))
+pinecone.init(api_key=st.secrets["PINECONE_API_KEY"], environment=st.secrets["PINECONE_ENVIRONMENT"])
 
 index = pinecone.Index(index_name)
 docsearch = Pinecone(index, embeddings.embed_query, "text")
